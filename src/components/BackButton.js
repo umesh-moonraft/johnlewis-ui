dd4781e13fde9ebc743aceb1f428b5845f3013e9
umesh-moonraft <umesh.patil@moonraft.com>
2023-05-04 21:51:02 +0530

    code changes

---
+++ src/components/BackButton.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function BackButton() {
+  return (
+    <div className="flex mb-4">
+      <Link to="/">
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
+            />
+          </svg>
+
+          <span className="sr-only">Back button</span>
+        </button>
+      </Link>
+    </div>
+  );
+}
