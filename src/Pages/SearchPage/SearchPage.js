c86e33552ebd41a0884f721a76c8384aeb9ff0ca
arorapuneet04 <35191971+arorapunu4@users.noreply.github.com>
2023-05-05 16:44:20 +0530

    Local host changes

--- src/Pages/SearchPage/SearchPage.js
+++ src/Pages/SearchPage/SearchPage.js
@@ -18,10 +18,18 @@ function SearchPage() {
     var requestOptions = {
       method: "GET",
       redirect: "follow",
+      headers: {
+        "ngrok-skip-browser-warning": true,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+      }
     };
-
+    //https://yellow-onions-fail-202-83-29-86.loca.lt/compare/jigsaw%20Supima%20Cotton%20T-Shirt
+    // fetch(
+    //   "http://localhost:3000/compare/" + encodeURIComponent(searchText),
+    //   requestOptions
+    // )
     fetch(
-      "http://localhost:3000/compare/" + encodeURIComponent(searchText),
+      "https://05a9-115-244-211-54.ngrok-free.app/compare/" + encodeURIComponent(searchText),
       requestOptions
     )
       .then((response) => response.json())
